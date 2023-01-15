223edb5ce5f12e7aecd81c68a9478afd204acdcd
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2023-01-14 20:09:24 -0500

    first commit

---
+++ src/assets/productData.js
@@ -0,0 +1,68 @@
+export const productData = [
+        {
+            product: "suv",
+            options: [
+                {
+                    company: "Tesla",
+                    company_logo: "Test",
+                    optionImage: "Test",
+                    optionDescription: "Test",
+                    optionCarbonEmissions: 10000,
+                    productLink:"",
+                    stages: {
+                        primary: "Test",
+                        secondary: "Test",
+                        tertiary: "Test"
+                    }
+                },
+                {
+                    company: "Ford",
+                    company_logo: "Test",
+                    optionImage: "Test",
+                    optionDescription: "Test",
+                    optionCarbonEmissions: 10000,
+                    stages: {
+                        primary: "Test",
+                        secondary: "Test",
+                        tertiary: "Test"
+                    }
+                },
+                {
+                    company: "Chevy",
+                    company_logo: "Test",
+                    optionImage: "Test",
+                    optionDescription: "Test",
+                    optionCarbonEmissions: 10000,
+                    stages: {
+                        primary: "Test",
+                        secondary: "Test",
+                        tertiary: "Test"
+                    }
+                },
+                {
+                    company: "Honda",
+                    company_logo: "Test",
+                    optionImage: "Test",
+                    optionDescription: "Test",
+                    optionCarbonEmissions: 10000,
+                    stages: {
+                        primary: "Test",
+                        secondary: "Test",
+                        tertiary: "Test"
+                    }
+                },
+                {
+                    company: "Ferrari",
+                    company_logo: "Test",
+                    optionImage: "Test",
+                    optionDescription: "Test",
+                    optionCarbonEmissions: 10000,
+                    stages: {
+                        primary: "Test",
+                        secondary: "Test",
+                        tertiary: "Test"
+                    }
+                }
+            ]
+        }
+    ]
